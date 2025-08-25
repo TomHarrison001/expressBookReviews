78353b593fa341b52868e755e99049ded5a32687
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-08-25 18:41:26 +0100

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,8 +38,15 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  const filteredBooks = Object.values(books).filter(
+    (book) => book.title === title
+  );
+  if (filteredBooks.length > 0) {
+    res.status(200).send(JSON.stringify(filteredBooks, null, 2));
+  } else {
+    res.status(404).json({ message: "Books with this title not found" });
+  }
 });
 
 //  Get book review
