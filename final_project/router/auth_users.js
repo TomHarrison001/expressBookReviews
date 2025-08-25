d1fd8442abccffeffb03e37d17d4dd683f2e6d97
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-08-25 20:13:06 +0100

    Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -66,6 +66,12 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(201).json({ message: "Review added" });
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const filteredBook = books[isbn];
+  return res.status(201).json({ message: "Review deleted" });
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
